48935424d615fe65c674d2b942b8cb28b9a4ba79
Gehadazzam <gehadgamalazzam@gmail.com>
2024-12-07 00:32:04 +0200

    feat: show countries

--- src/views/exploreCountries.vue
+++ src/views/exploreCountries.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="">
-    <h1>Hello countries</h1>
+  <div class="bg-lightMood dark:bg-darkMood text-lightText dark:text-white px-24">
+    <div class="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 md:gap-8 lg:gap-12">
+      <div
+        v-for="(country, index) in countries"
+        :key="index"
+        class="bg-white rounded-md pb-6 dark:bg-DarkBlue"
+      >
+        <img
+          class="rounded-t-md h-40 w-full pb-4"
+          :src="country.flags.png"
+          alt="`${country.name.common} flag`"
+        />
+        <h2 class="font-bold pl-6">{{ country.name.common }}</h2>
+        <div class="pl-6">
+          <h3 class="font-semibold">
+            Population: <span>{{ country.population }}</span>
+          </h3>
+          <h3 class="font-semibold">
+            Region: <span>{{ country.region }}</span>
+          </h3>
+          <h3 v-if="country.capital && country.capital.length > 0" class="font-semibold">
+            Capital: <span>{{ country.capital[0] }}</span>
+          </h3>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import api from '../services/api'
+import { ref } from 'vue'
+
+const countries = ref([])
+
+const fetchData = async () => {
+  try {
+    const res = await api.get('/all')
+    console.log('Response:', res)
+    console.log('Data:', res.data)
+    countries.value = res.data
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
+fetchData()
+</script>
--- src/views/theHeader.vue
+++ src/views/theHeader.vue
@@ -5,9 +5,9 @@ import themeSwitcher from '../components/themeSwitcher.vue'
 <template>
   <header>
     <div
-      class="flex items-center justify-between px-4 md:px-12 lg:px-24 py-2 bg-VeryLightGray dark:bg-veryDarkBlue"
+      class="flex items-center justify-between px-4 md:px-12 lg:px-24 py-2 shadow-[0px_8px_16px_-2px_rgba(0,_0,_0,_0.1)] bg-lightMood dark:bg-darkMood"
     >
-      <h3 class="text-VeryDarkBlue dark:text-white">Where in the world?</h3>
+      <h3 class="text-lightText dark:text-white">Where in the world?</h3>
       <theme-switcher />
     </div>
   </header>
